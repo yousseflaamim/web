a38509f18b7bfdb73c716f1b0ee999ce76541e42
yousseflaamim <39153351+yousseflaamim@users.noreply.github.com>
2023-07-06 00:17:34 +0200

    Update script.js
    updt

--- wbp2/script.js
+++ wbp2/script.js
@@ -1,23 +1,38 @@
-// script.js
+/**Author: Youssef Laamim
+  Copyright (c) 2023. All rights reserved. */
 
 document.getElementById('servings').addEventListener('input', updateIngredients);
 document.getElementById('add-review-btn').addEventListener('click', addReview);
-
 function updateIngredients() {
-    const servingsInput = document.getElementById('servings');
-    const servings = parseInt(servingsInput.value);
+  const servingsInput = document.getElementById('servings');
+  const servings = parseInt(servingsInput.value);
 
-    const ingredientsList = document.getElementById('ingredients-list');
-    const ingredients = ingredientsList.getElementsByTagName('li');
+  const ingredientsList = document.getElementById('ingredients-list');
+  const ingredients = ingredientsList.getElementsByTagName('li');
 
-    for (let i = 0; i < ingredients.length; i++) {
-        const ingredient = ingredients[i].textContent;
-        const quantity = parseInt(ingredient.split(' ')[0]);
-        const updatedQuantity = quantity * servings;
+  const originalIngredients = [
+      '100 g Roasted hazelnuts',
+      '250 g Svenskt Smör från Arla®',
+      '2 dl Kakao',
+      '2 Egg',
+      '4 1/2 Powdered sugar',
+      '1/2tsk Salt',
+      '2 krm Vanilla powder',
+      '3 dl Flour',
+      '1/2 tsk Baking Soda'
+  ];
 
-        ingredients[i].textContent = updatedQuantity + ingredient.substring(ingredient.indexOf(' '));
-    }
+  for (let i = 0; i < ingredients.length; i++) {
+      const ingredientParts = originalIngredients[i].split(' ');
+      const quantity = parseInt(ingredientParts[0]);
+      const unit = ingredientParts.slice(1).join(' ');
+
+      const updatedQuantity = quantity * servings;
+      ingredients[i].textContent = `${updatedQuantity} ${unit}`;
+  }
 }
+
+
 function addReview() {
     const reviewInput = document.getElementById('review-input');
     const reviewText = reviewInput.value;
@@ -28,27 +43,28 @@ function addReview() {
         newReview.textContent = reviewText;
         reviewsList.appendChild(newReview);
 
-        reviewInput.value = ''; // لإعادة تفريغ حقل إدخال المراجعة
+        reviewInput.value = '';
     }
 }
-//moveimg
+
+
 const images = ['img/lakrim.jpg', 'img/kika2.jpg', 'img/baghrir.jpg'];
 let currentImageIndex = 0;
 const recipeImages = document.querySelectorAll('.recipe-images img');
 
 function changeImage() {
-  recipeImages.forEach((image, index) => {
-    if (index === currentImageIndex) {
-      image.style.display = 'block';
-    } else {
-      image.style.display = 'none';
-    }
-  });
+    recipeImages.forEach((image, index) => {
+        if (index === currentImageIndex) {
+            image.style.display = 'block';
+        } else {
+            image.style.display = 'none';
+        }
+    });
 
-  currentImageIndex++;
-  if (currentImageIndex >= images.length) {
-    currentImageIndex = 0;
-  }
+    currentImageIndex++;
+    if (currentImageIndex >= images.length) {
+        currentImageIndex = 0;
+    }
 }
 
-setInterval(changeImage, 10000); // تغيير الصورة كل 10 ثوانٍ
+setInterval(changeImage, 10000);
